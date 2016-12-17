f428e4adf1ed08e396e8a1d3dbbaa3aaeda7de84
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-12-17 19:59:14 +0300

    move to packages

--- src/ru/mail/polis/hashtable/ChainHashTable.java
+++ src/ru/mail/polis/hashtable/ChainHashTable.java
@@ -1,4 +1,6 @@
-package ru.mail.polis;
+package ru.mail.polis.hashtable;
+
+import ru.mail.polis.ISet;
 
 import java.util.ArrayList;
 import java.util.Comparator;
--- src/ru/mail/polis/tree/BinarySearchTree.java
+++ src/ru/mail/polis/tree/BinarySearchTree.java
@@ -1,4 +1,6 @@
-package ru.mail.polis;
+package ru.mail.polis.tree;
+
+import ru.mail.polis.ISortedSet;
 
 import java.util.ArrayList;
 import java.util.Comparator;

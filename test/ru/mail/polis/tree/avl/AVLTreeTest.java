8a6aff2247f1d0b965fae2865681fb408560e161
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-12-17 21:51:36 +0300

    test add

--- test/ru/mail/polis/tree/avl/AVLTreeTest.java
+++ test/ru/mail/polis/tree/avl/AVLTreeTest.java
@@ -44,4 +44,60 @@ public class AVLTreeTest {
 
         // assertThat(tree.remove(SOME_NODE), is(true));
     }
+
+    @Test
+    public void addTwoSame() throws Exception {
+        final AVLTree<Integer> tree = new AVLTree<>();
+
+        assertThat(tree.add(SOME_NODE), is(true));
+        assertThat(tree.add(SOME_NODE), is(false));
+    }
+
+    @Test
+    public void addTwoDifferent() throws Exception {
+        final AVLTree<Integer> tree = new AVLTree<>();
+
+        assertThat(tree.add(SOME_NODE), is(true));
+        assertThat(tree.add(100), is(true));
+    }
+
+    @Test
+    public void add3AndRotate() throws Exception {
+        final AVLTree<Integer> tree = new AVLTree<>();
+        assertThat(tree.add(3), is(true));
+        assertThat(tree.add(2), is(true));
+        assertThat(tree.add(1), is(true));
+
+        final String treeString = AvlBuilder.serialize(tree);
+        final String expectedTree = "2 1 -1 -1 3 -1 -1";
+        assertThat(treeString, is(expectedTree));
+    }
+
+    @Test
+    public void add4NodesAndRotate() throws Exception {
+        final AVLTree<Integer> tree = new AVLTree<>();
+        assertThat(tree.add(4), is(true));
+        assertThat(tree.add(3), is(true));
+        assertThat(tree.add(2), is(true));
+        assertThat(tree.add(1), is(true));
+
+        final String treeString = AvlBuilder.serialize(tree);
+        final String expectedTree = "3 2 1 -1 -1 -1 4 -1 -1";
+        assertThat(treeString, is(expectedTree));
+    }
+
+    @Test
+    public void add4andFind() throws Exception {
+        final AVLTree<Integer> tree = new AVLTree<>();
+        tree.add(4);
+        tree.add(3);
+        tree.add(2);
+        tree.add(1);
+
+        for (int i = 1; i <= 4; i++) {
+            assertThat(tree.contains(i), is(true));
+        }
+        assertThat(tree.contains(0), is(false));
+        assertThat(tree.contains(5), is(false));
+    }
 }

bceb1ec7d8aa6ca620f94907261956c026fdca22
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-12-22 17:01:11 +0300

    refactor instanceof

--- src/ru/mail/polis/hashtable/Hash.java
+++ src/ru/mail/polis/hashtable/Hash.java
@@ -2,7 +2,7 @@ package ru.mail.polis.hashtable;
 
 class Hash {
     static <E> int h1(E value) {
-        if (String.class.isInstance(value)) {
+        if (value instanceof String) {
             return StringHash.h1((String) value);
         } else {
             throw new UnsupportedOperationException(
@@ -11,7 +11,7 @@ class Hash {
     }
 
     static <E> int h2(E value, int m) {
-        if (String.class.isInstance(value)) {
+        if (value instanceof String) {
             return StringHash.h2((String) value, m);
         } else {
             throw new UnsupportedOperationException(
